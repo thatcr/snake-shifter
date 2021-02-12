4eac1d17a26b77f7b19c15b830eec769673af637
Rob Thatcher <thatcr@gmail.com>
2021-02-12 15:12:43 +0000

    add abc, fix mypy

--- src/snake/shifter/context.py
+++ src/snake/shifter/context.py
@@ -1,36 +1,76 @@
 """Context manager and empty handler."""
+from abc import ABCMeta
+from abc import abstractmethod
+from types import TracebackType
+from typing import Any
+from typing import List
+from typing import Literal
+from typing import Optional
+from typing import Tuple
+from typing import Type
+
+
+class CallKey(Tuple[Any, ...]):
+    """A call key refers to a function call and is a tupleof it's arguments."""
+
+    pass
+
+
+class CallHandler(metaclass=ABCMeta):
+    """Handler invoked from node functions when they are called."""
+
+    @abstractmethod
+    def __contains__(self, key: CallKey) -> bool:
+        """Test to see if this call should be handled by the __getitem__."""
+        return False
+
+    @abstractmethod
+    def __getitem__(self, key: CallKey) -> Any:
+        """Return the value to return for a call."""
+        return NotImplemented
+
+    @abstractmethod
+    def __setitem__(self, key: CallKey, value: Any) -> None:
+        """Receive the value calculated for a call."""
+        return None
 
 
 class Context(object):
     """Context handler that maintains a stack of handlers."""
 
-    def __init__(self, handler):
+    def __init__(self, handler: CallHandler):
         """Initialize context with a handler instance."""
         self.handler = handler
 
-    def __enter__(self):
+    def __enter__(self) -> CallHandler:
         """Push the handler onto the global stack."""
         global _handlers
         _handlers.append(self.handler)
         return self.handler
 
-    def __exit__(self, exc_type, exc_value_, traceback):
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        traceback: Optional[TracebackType],
+    ) -> Literal[False]:
         """Remove handler from the global stack."""
         _handlers.pop(-1)
+        return False
 
 
-class NullHandler(object):
+class NullHandler(CallHandler):
     """Handler that does nothing."""
 
-    def __contains__(self, key):
+    def __contains__(self, key: CallKey) -> bool:
         """Avoid handling anything."""
         return False
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: CallKey) -> Any:
         """We never cache any values on this handler."""
         raise NotImplementedError()
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: CallKey, value: Any) -> None:
         """Never process any function results."""
         pass
 
@@ -38,9 +78,9 @@ class NullHandler(object):
 # global stack of handlers, tail element is used for t
 # the active handler.
 # TODO make this thread/coroutine safe
-_handlers = [NullHandler()]
+_handlers: List[CallHandler] = [NullHandler()]
 
 
-def get_handler():
+def get_handler() -> CallHandler:
     """Return the active handler at the top of the stack."""
     return _handlers[-1]
--- src/snake/shifter/wrapper.py
+++ src/snake/shifter/wrapper.py
@@ -7,9 +7,13 @@ benchmark implementation to test the other approaches against.
 import functools
 import inspect
 from collections import namedtuple
+from typing import Any
+from typing import Callable
+
+from .context import CallKey
 
 
-def node(func):
+def node(func: Callable[..., Any]) -> Callable[..., Any]:
     """Wrap a function with calls to a handler to modify it's behaviour.
 
     Args:
@@ -20,31 +24,35 @@ def node(func):
     """
     sig = inspect.signature(func)
 
-    key_type = namedtuple(
-        func.__name__,
-        sig.parameters.keys(),
-        module=func.__module__,
-    )
-    key_type.func = func
-
     # patch the repr so we display the full function name
     repr_fmt = "(" + ", ".join(f"{name}=%r" for name in sig.parameters.keys()) + ")"
 
-    def _repr(self):
+    def _repr(self: Any) -> Any:
         return self.__module__ + "." + self.__class__.__name__ + repr_fmt % self
 
-    key_type.__repr__ = _repr
+    key_type = type(
+        func.__name__,
+        (
+            namedtuple(
+                func.__name__,
+                sig.parameters.keys(),
+                module=func.__module__,
+            ),
+            CallKey,
+        ),
+        {"__repr__": _repr, "__func__": func, "__module__": func.__module__},
+    )
 
     # import the global context handler stack here, which we bind into the wrapper
     from .context import _handlers
 
     @functools.wraps(func)
-    def _func(*args, **kwargs):
+    def _func(*args: Any, **kwargs: Any) -> Any:
         handler = _handlers[-1]
 
         bound = sig.bind(*args, **kwargs)
         bound.apply_defaults()
-        key = key_type._make(bound.args)
+        key = key_type(*bound.args)
 
         if key in handler:
             value = handler[key]
@@ -60,5 +68,5 @@ def node(func):
             handler[key] = Exception(exc)
             raise
 
-    _func.key = key_type
+    _func.key = key_type  # type: ignore
     return _func
