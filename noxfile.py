2339d27a5f4095e8b143739acbe64e50251cd277
Rob Thatcher <thatcr@gmail.com>
2021-02-24 13:11:18 +0000

    re-use coverage

--- noxfile.py
+++ noxfile.py
@@ -159,7 +159,7 @@ def coverage(session: Session) -> None:
 def typeguard(session: Session) -> None:
     """Runtime type checking using Typeguard."""
     session.install(".")
-    session.install("pytest", "typeguard", "pygments")
+    session.install("pytest", "typeguard", "pygments", "xdoctest[color]")
     session.run("pytest", f"--typeguard-packages={package}", *session.posargs)
 
 
