fe00364cb0620990f6fbf8061aa53557d37d2039
Rob Thatcher <thatcr@gmail.com>
2021-03-03 10:45:13 +0000

    fix typeguard

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,13 @@
 """Common fixures for testing snake-shifter."""
 from _pytest.python import Metafunc
 
-import snake.shifter.wrapper
-
-
-decorators = [snake.shifter.wrapper.shift]
-
 
 def pytest_generate_tests(metafunc: Metafunc) -> None:
     """Map decorator fixture to list of decorators to test."""
+    import snake.shifter.wrapper
+
+    decorators = [snake.shifter.wrapper.shift]
+
     if "decorator" in metafunc.fixturenames:
         metafunc.parametrize(
             "decorator", decorators, ids=[d.__module__ for d in decorators]
