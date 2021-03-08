535dbd5782e436b5ff6f8cba66b0e85d26e5cdf6
Rob Thatcher <thatcr@gmail.com>
2021-03-08 10:05:11 +0000

    add more repr tests add (broken) tests for args, kwargs

--- tests/test_key_type.py
+++ tests/test_key_type.py
@@ -1,4 +1,6 @@
 """Check that constructed key types work correctly."""
+from typing import Optional
+
 from snake.shifter.key_type import make_key_type
 
 
@@ -36,9 +38,35 @@ def test_equality() -> None:
 def test_repr() -> None:
     """Check we get a nice printable signature."""
 
-    def f(a: int, b: int) -> int:
+    def f(a: int, b: int, c: int = 3, d: Optional[None] = None) -> int:
         ...
 
     f_type = make_key_type(f)
 
-    assert repr(f_type(1, 2)) == "tests.test_key_type.f(a=1, b=2)"
+    assert repr(f_type(1, 2)) == "tests.test_key_type.f(a=1, b=2, c=3, d=None)"
+    assert repr(f_type(1, 2, 5)) == "tests.test_key_type.f(a=1, b=2, c=5, d=None)"
+    assert repr(f_type(1, 2, c=4)) == "tests.test_key_type.f(a=1, b=2, c=4, d=None)"
+    assert repr(f_type(1, 2, d=10)) == "tests.test_key_type.f(a=1, b=2, c=3, d=10)"
+    assert repr(f_type(1, 2, c=4, d=10)) == "tests.test_key_type.f(a=1, b=2, c=4, d=10)"
+
+
+def test_args() -> None:
+    """Verify that positional variable arguments work."""
+
+    def f(*args) -> int:
+        ...
+
+    key_type = make_key_type(f)
+
+    assert repr(key_type(1, 2)) == "test.test_key_type.f(a=1, b=2)"
+
+
+def test_kwargs() -> None:
+    """Verify that keyword variable arguments work."""
+
+    def f(**kwargs) -> int:
+        ...
+
+    key_type = make_key_type(f)
+
+    assert repr(key_type(1, 2)) == "test.test_key_type.f(a=1, b=2)"
