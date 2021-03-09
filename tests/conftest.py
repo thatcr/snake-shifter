3db101378ede941f00467a9c3a94b2a52d5c0ecf
Rob Thatcher <thatcr@gmail.com>
2021-03-09 09:37:55 +0000

    override print to use rich

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,9 @@
 """Common fixures for testing snake-shifter."""
+import sys
+
+import pytest
 from _pytest.python import Metafunc
+from rich.console import Console
 
 
 def pytest_generate_tests(metafunc: Metafunc) -> None:
@@ -12,3 +16,20 @@ def pytest_generate_tests(metafunc: Metafunc) -> None:
         metafunc.parametrize(
             "decorator", decorators, ids=[d.__module__ for d in decorators]
         )
+
+
+@pytest.fixture(autouse=True, scope="session")
+def _rich_print(request):
+    """Create a full-color rich terminal for logging."""
+    request.console = Console(
+        force_terminal=True,
+        force_interactive=False,
+        file=sys.stdout,
+        record=False,
+    )
+    old_print = __builtins__["print"]
+    __builtins__["print"] = request.console.print
+
+    yield request.console.print
+
+    __builtins__["print"] = old_print
